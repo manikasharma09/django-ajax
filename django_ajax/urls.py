bdc65346da0dcd33f262b4f0307b3662005ae630
Manika Sharma <51908554+manikasharma09@users.noreply.github.com>
2019-06-22 22:30:50 +0530

    Add files via upload

---
+++ django_ajax/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+from django.urls import path,include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',include('app.urls')),
+]
